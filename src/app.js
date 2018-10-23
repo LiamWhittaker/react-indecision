57d24c9bf2156aac4e22a684275816d81bf6b73c
Liam Whittaker <liamgwhittaker@gmail.com>
2018-10-23 17:07:29 +0100

    finished playground

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 console.log('app.js is running');
 
-var app = {
+const app = {
   title: 'Indecision App',
   subtitle: 'A React app to help you decide stuff',
   options: ['One', 'Two']
 }
 
-var template = (
+const template = (
   <div>
     <h1>{app.title}</h1> 
     {app.subtitle && <p>{app.subtitle}</p>}
@@ -17,11 +17,11 @@ var template = (
     </ol>
   </div>
 );
-var userName = 'Liam';
-var userAge = 29;
-var userLocation = 'The Moon';
+const userName = 'Liam';
+const userAge = 29;
+const userLocation = 'The Moon';
 
-var user = {
+const user = {
   name: 'Liam',
   age: 18,
   location: 'Valhalla'
@@ -33,7 +33,7 @@ function getLocation(location) {
   } 
 }
 
-var templateTwo = (
+const templateTwo = (
   <div>
     <h1>{ (user.name) ? user.name : 'Anon' }</h1>
     { (user.age && user.age >= 18) && <p>Age: { user.age }</p> }
@@ -41,7 +41,7 @@ var templateTwo = (
   </div>
 );
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 // Takes 2 args: JSX and where to render it
 ReactDOM.render(template, appRoot);
